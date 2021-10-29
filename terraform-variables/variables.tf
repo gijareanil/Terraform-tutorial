a4c1efb43f2ebac3c50c08866520e79859c5d732
gijareanil <93199787+gijareanil@users.noreply.github.com>
2021-10-29 21:00:44 +0530

    Update variables.tf

--- terraform-variables/variables.tf
+++ terraform-variables/variables.tf
@@ -10,8 +10,8 @@ default="t2.micro"
 variable "ami_id" {
 type = "map"
 default = {
-us-east-1 = "ami-035b3c7efe6d061d5"
-eu-west-2= "ami-132b3c7efe6sdfdsfd"
-eu-central-1="ami-9787h5h6nsn"
+ap-south-1 = "ami-035b3c7efe6d061d5"
+ap-south-1= "ami-132b3c7efe6sdfdsfd"
+ap-south-1="ami-9787h5h6nsn"
 }
 }
